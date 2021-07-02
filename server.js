8e673897a5948a190ca96c6fb72984a8e80e4f10
Nikhil <nddubey15@gmail.com>
2021-07-02 14:47:21 +0530

    auth system

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const mongoose = require('mongoose')
 const session = require('express-session')
 const flash = require('express-flash')
 const MongoDbStore = require('connect-mongo')
+const passport = require('passport')
 
 
 
@@ -26,7 +27,16 @@ connection.once('open', () => {
 });
 
 
+
+
+
+app.use(flash())
+//Assests
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: false }))
 app.use(express.json())
+
+
 
 
 
@@ -58,15 +68,21 @@ app.use(session({
     cookie: { maxAge: 1000 * 60 * 60 *24 } //24 hours
 }))
 
+//Passport config
+const passportInit = require('./app/config/passport')
+passportInit(passport)
+app.use(passport.initialize())
+app.use(passport.session())
+
+
 //Global Middleware
 app.use((req, res, next) =>{
     res.locals.session = req.session
+    res.locals.user = req.user
     next()
 })
 
-app.use(flash())
-//Assests
-app.use(express.static('public'))
+
 
 
 require('./routes/web')(app)
