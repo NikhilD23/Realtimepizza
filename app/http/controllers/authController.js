c3d95cd5f782868df825da8bdb9df55ef56fa1e8
Nikhil <nddubey15@gmail.com>
2021-07-04 02:23:36 +0530

    add orders table, place order;

--- app/http/controllers/authController.js
+++ app/http/controllers/authController.js
@@ -3,6 +3,10 @@ const bcrypt = require('bcrypt')
 const passport = require('passport')
 function authController(){
 
+    const _getRedirectUrl = (req) => {
+        return req.user.role === 'admin' ? '/admin/orders' : '/customer/orders'
+    }
+
     return{
 
         login(req, res){
@@ -38,7 +42,7 @@ function authController(){
 
                     }
 
-                    return res.redirect('/')
+                    return res.redirect(_getRedirectUrl(req))
 
                 })
 
